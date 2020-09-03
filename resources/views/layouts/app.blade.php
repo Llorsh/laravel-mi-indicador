64f6532e5ba3e4885106e1a47f0f5b6566908870
Llorsh <jorge.m.arias.p@gmail.com>
2020-09-03 02:29:55 -0400

    03.09.2020 02.29

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a href="{{ url('grafico') }}" class="nav-link">Graficos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('grafico/uf') }}" class="nav-link">Grafico UF</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
